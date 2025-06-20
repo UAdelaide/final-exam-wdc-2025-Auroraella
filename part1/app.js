bb83c142b13b1124044b83be3f946d020b4be6ca
Auroraella <banmaihn05@gmail.com>
2025-06-20 08:45:37 +0000

    [automatic commit] 2025-06-20 08:45:37 UTC

--- part1/app.js
+++ part1/app.js
@@ -126,7 +126,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
     `);
     res.json(rows);
   } catch (err) {
-    res.status(500).json({ error: })
+    res.status(500).json({ error: 'Failed to fetch open walk requests'});
   }
 });
 
