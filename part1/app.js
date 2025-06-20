9df13ce2838ea78250fb206675608db3a07c4eda
Auroraella <banmaihn05@gmail.com>
2025-06-20 08:21:51 +0000

    [automatic commit] 2025-06-20 08:21:51 UTC

--- part1/app.js
+++ part1/app.js
@@ -49,7 +49,7 @@ let db;
     const [dogRows] = await db.execute('SELECT COUNT(*) AS count FROM Dogs');
     if (dogRows[0].count) === 0) {
       const [owners] = await db.execute("SELECT user_id, username FROM Users WHERE role='owner'");
-      
+      const alice = owners.find(u =>)
     }
   }
 })();
