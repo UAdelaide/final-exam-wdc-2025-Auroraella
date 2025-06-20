8a6c78b50199d431f3217a6673bff2a29088a428
Auroraella <banmaihn05@gmail.com>
2025-06-20 08:40:16 +0000

    [automatic commit] 2025-06-20 08:40:16 UTC

--- part1/app.js
+++ part1/app.js
@@ -105,7 +105,8 @@ app.get('/api/dogs', async (req, res) => {
         SELECT d.name AS dog_name, d.size, u.username AS owner_username
         FROM Dogs d
         JOIN Users u ON d.owner_id = u.user_id
-        `);
+    `);
+    
   }
   });
 module.exports = app;
