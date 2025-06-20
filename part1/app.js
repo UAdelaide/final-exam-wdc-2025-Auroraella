a03ede07eb641983fbfe169b45960571937a33a0
Auroraella <banmaihn05@gmail.com>
2025-06-20 08:31:14 +0000

    [automatic commit] 2025-06-20 08:31:14 UTC

--- part1/app.js
+++ part1/app.js
@@ -68,6 +68,8 @@ let db;
     const candy = dogs.find(d => d.name === 'Candy');
     const apple = dogs.find(d => d.name === 'Apple');
     await db.execute(`INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
+      
+    `);
   }
 })();
 // catch 404 and forward to error handler
