37ee0aa0a2c7b01cec233e236f63a4b37b70c9c5
Auroraella <banmaihn05@gmail.com>
2025-06-20 15:04:06 +0000

    [automatic commit] 2025-06-20 15:04:06 UTC

--- part2/app.js
+++ part2/app.js
@@ -7,7 +7,9 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
-app.use
+app.use(session({
+secret: process.env.SESSION_SECRET || 'dogwalksecret',
+}));
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
