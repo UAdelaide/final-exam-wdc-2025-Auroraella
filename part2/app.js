40461835113b1cfe4d19f0527c55cae9c04c68be
Auroraella <banmaihn05@gmail.com>
2025-06-20 15:04:26 +0000

    [automatic commit] 2025-06-20 15:04:26 UTC

--- part2/app.js
+++ part2/app.js
@@ -8,7 +8,8 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(session({
-secret: process.env.SESSION_SECRET || 'dogwalksecret',
+    secret: process.env.SESSION_SECRET || 'dogwalksecret',
+    resave
 }));
 
 // Routes
