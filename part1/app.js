3f365b2c17111739aa9403bc7a2aec17a0b32af4
Auroraella <banmaihn05@gmail.com>
2025-06-20 08:48:38 +0000

    [automatic commit] 2025-06-20 08:48:38 UTC

--- part1/app.js
+++ part1/app.js
@@ -146,7 +146,7 @@ app.get('/api/walkers/summary', async (req, res) => {
     `);
     res.json(rows);
   } catch (err) {
-    res.status(500).json()
+    res.status(500).json({ error: 'Failed to fetch walker summary'})
   }
 
 });
