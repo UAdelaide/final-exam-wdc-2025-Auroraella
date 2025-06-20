e4ff2545ef5ae67788af9eea147064bb04b4e123
Auroraella <banmaihn05@gmail.com>
2025-06-20 08:10:08 +0000

    [automatic commit] 2025-06-20 08:10:08 UTC

--- part1/app.js
+++ part1/app.js
@@ -36,16 +36,18 @@ let db;
 
     // Q%; SQL insert into
     const [userRows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
-    if (userRows[0].count ===
+    if (userRows[0].count === 0 {
+
+    }
   }
 })();
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
