628ed9ee8af63be3e3f08e7320edffd088cb43ce
Auroraella <banmaihn05@gmail.com>
2025-06-20 08:44:57 +0000

    [automatic commit] 2025-06-20 08:44:57 UTC

--- part1/app.js
+++ part1/app.js
@@ -124,6 +124,9 @@ app.get('/api/walkrequests/open', async (req, res) => {
       JOIN Users u ON d.owner_id = u.user_id
       WHERE wr.status = 'open'
     `);
+    res.json(rows);
+  } catch (err) {
+    r
   }
 });
 
