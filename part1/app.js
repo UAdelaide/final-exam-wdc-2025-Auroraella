c1703bf106f09cca73bc6c71eacf8a20ceaa4412
Auroraella <banmaihn05@gmail.com>
2025-06-20 08:11:29 +0000

    [automatic commit] 2025-06-20 08:11:29 UTC

--- part1/app.js
+++ part1/app.js
@@ -37,7 +37,8 @@ let db;
     // Q%; SQL insert into
     const [userRows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
     if (userRows[0].count === 0 {
-      await db.execute('INSERT INTO Users (username, email, password_hash, role) VALUES')
+      await db.execute('INSERT INTO Users (username, email, password_hash, role) VALUES
+      ');
     }
   }
 })();
