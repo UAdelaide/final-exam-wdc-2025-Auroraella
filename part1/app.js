81323d5b38287fd5e52a9b08f09125af95abf842
Auroraella <banmaihn05@gmail.com>
2025-06-20 08:23:12 +0000

    [automatic commit] 2025-06-20 08:23:12 UTC

--- part1/app.js
+++ part1/app.js
@@ -51,7 +51,7 @@ let db;
       const [owners] = await db.execute("SELECT user_id, username FROM Users WHERE role='owner'");
       const alice = owners.find(u => u.username === 'alice123');
       const carol = owners.find(u => u.username === 'carol123');
-      const mina = 
+      const mimi = owners.find(u => )
     }
   }
 })();
