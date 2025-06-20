c295f82206667cf2a4ced0b61ada49088b31f181
Auroraella <banmaihn05@gmail.com>
2025-06-20 08:32:14 +0000

    [automatic commit] 2025-06-20 08:32:14 UTC

--- part1/app.js
+++ part1/app.js
@@ -69,6 +69,8 @@ let db;
     const apple = dogs.find(d => d.name === 'Apple');
     await db.execute(`INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
       (${max.dog_id}, '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+      (${bella.dog_id}, '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
+      
     `);
   }
 })();
