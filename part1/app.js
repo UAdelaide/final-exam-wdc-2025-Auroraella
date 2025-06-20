1080a64f5e351210a4e40a0b487226456f983321
Auroraella <banmaihn05@gmail.com>
2025-06-20 09:23:07 +0000

    [automatic commit] 2025-06-20 09:23:07 UTC

--- part1/app.js
+++ part1/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var mysql 
+var mysql = require('mysql2/promise');
 var app = express();
 
 // view engine setup
