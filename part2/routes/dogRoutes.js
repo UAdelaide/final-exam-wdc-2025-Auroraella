00cfd624aea8c3615c8c49f03b2eb898b81d8a5a
Auroraella <banmaihn05@gmail.com>
2025-06-20 17:04:42 +0000

    [automatic commit] 2025-06-20 17:04:42 UTC

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -6,5 +6,11 @@ const db = require('../models/db');
 router.get('/', async (req, res) => {
     try {
         const [rows] = await db.query(`
+            SELECT dog_id as id, name, size, owner_id FROM Dogs
+        `);
+        res.json(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to load dogs' })
     }
 }
