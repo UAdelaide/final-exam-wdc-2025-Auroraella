202d4eca663c62842679dde3c87c723c924e19d4
Auroraella <banmaihn05@gmail.com>
2025-06-20 08:14:30 +0000

    [automatic commit] 2025-06-20 08:14:30 UTC

--- part1/app.js
+++ part1/app.js
@@ -39,7 +39,7 @@ let db;
     if (userRows[0].count === 0) {
       await db.execute('INSERT INTO Users (username, email, password_hash, role) VALUES
         ('alice', 'alice@example.com', 'hased123', 'owner'),
-        ('bobwalker', )
+        ('bobwalker', 'bob@example.com', 'hased')
         ');
     }
   }
