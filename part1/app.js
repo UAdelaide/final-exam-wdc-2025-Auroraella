513c2e4b78c7934e6c94f73235bd7b99a8944d34
Auroraella <banmaihn05@gmail.com>
2025-06-20 09:22:27 +0000

    [automatic commit] 2025-06-20 09:22:27 UTC

--- part1/app.js
+++ part1/app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const { mkdtempSync } = require('fs');
 
 var app = express();
 
