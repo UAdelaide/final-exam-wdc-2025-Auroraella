b140e86c04a52ae6f189de6056141eb36230c43f
Auroraella <banmaihn05@gmail.com>
2025-06-21 02:24:17 +0000

    [automatic commit] 2025-06-21 02:24:17 UTC

--- part2/app.js
+++ part2/app.js
@@ -35,7 +35,12 @@ function requireOwner(req, res, next) {
 function requireWalker(req, res, next) {
     if (!req.session.user) {
         return res.redirect('/index.html');
-
+    }
+    if (req.session.user.role !== 'walker') {
+        return res.status(403).json({ error: 'Forbidden' });
+    }
+    next();
+}
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
