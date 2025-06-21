56d7cefba06d3a6e9fc408e6cee663bc318113f9
Auroraella <banmaihn05@gmail.com>
2025-06-21 02:48:24 +0000

    [automatic commit] 2025-06-21 02:48:24 UTC

--- part2/app.js
+++ part2/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 function requireOwner(req, res, next) {
     if (!req.session.user) {
-        return res.redirect('/index');
+        return res.redirect('/');
     }
     if (req.session.user.role !== 'owner') {
         return res.status(403).json({ error: 'Forbidden' });
