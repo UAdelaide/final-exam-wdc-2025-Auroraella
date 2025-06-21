62e64e33a8fd0425dc8106bb5c23a1161ab3757f
Auroraella <banmaihn05@gmail.com>
2025-06-21 02:58:06 +0000

    [automatic commit] 2025-06-21 02:58:06 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -56,6 +56,7 @@ router.post('/login', async (req, res) => {
   }
 });
 
+
 // Q15: GET dogs for specific owner
 router.get('/:ownerId/dogs', async (req, res) => {
   const ownerId = req.params.ownerId;
